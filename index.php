198b386d9c792b637fda3764fce33a2cce07c912
khanh309 <voanlaptrinh@gmail.com>
2024-05-26 22:28:35 +0700

    main

--- index.php
+++ index.php
@@ -7,19 +7,39 @@ $clientIP = $_SERVER['REMOTE_ADDR'];
 // Lấy thông tin địa lý từ IP sử dụng dịch vụ của ipinfo.io
 $geoInfo = @file_get_contents("http://ipinfo.io/{$clientIP}/json");
 if ($geoInfo === FALSE) {
-    $geoInfo = json_encode(["error" => "Không thể lấy thông tin địa lý"]);
+    $geoInfo = ["error" => "Không thể lấy thông tin địa lý"];
 } else {
     $geoInfo = json_decode($geoInfo, true);
 }
+var_dump($geoInfo);
+
+// Lấy tên quốc gia từ thông tin địa lý
+$countryName = isset($geoInfo['country']) ? $geoInfo['country'] : 'Không có thông tin quốc gia';
 
 // Thông tin hệ điều hành của server
 $osInfo = php_uname();
 
+// Lấy tên máy chủ (hostname)
+$hostname = gethostname();
+
+// Lấy địa chỉ IP của máy chủ
+if (stristr(PHP_OS, 'win')) {
+    // Trên Windows
+    $serverIP = gethostbyname($hostname);
+} else {
+    // Trên Linux/Mac
+    $serverIP = shell_exec("hostname -I");
+    $serverIP = trim($serverIP);
+}
+
 // Tạo response
 $response = [
-    'ip' => $clientIP,
+    'client_ip' => $clientIP,
     'geo' => $geoInfo,
-    'os' => $osInfo
+    'country' => $countryName,
+    'os' => $osInfo,
+    'hostname' => $hostname,
+    'server_ip' => $serverIP
 ];
 
 // Trả về JSON response
