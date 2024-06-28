3a38ba0a7f7104cd23181771f29ed422e26a97d8
khanh <voanlaptrinh@gmail.com>
2024-06-28 16:31:30 +0700

    version

--- version.php
+++ version.php
@@ -6,32 +6,47 @@ $latestVersion = "1.2.0";
 $downloadUrl = "https://example.com/download/app-v1.2.0.apk";
 $releaseNotes = "Đã sửa lỗi và có bản cập nhật";
 
-if (isset($_GET['current_version'])) {
+$ytdlpVersion = "1.2.0";
+$ffmpegVersion = "1.2.0";
+
+if (isset($_GET['current_version']) && isset($_GET['ytdlp_version']) && isset($_GET['ffmpeg_version'])) {
     $currentVersion = $_GET['current_version'];
+    $ytdlp_version = $_GET['ytdlp_version'];
+    $ffmpeg_version = $_GET['ffmpeg_version'];
 
-    if (version_compare($currentVersion, $latestVersion, '!=')) {
-
-        $response = [
-            'update' => true,
-            'latest_version' => $latestVersion,
-            'download_url' => $downloadUrl,
-            'release_notes' => $releaseNotes
-        ];
-    } else {
-
-        $response = [
-            'update' => false,
-            'message' => 'Bạn đang sử dụng phiên bản mới nhất.'
-        ];
-    }
-} else {
+    $appUpdateAvailable = version_compare($currentVersion, $latestVersion, '!=');
+    $ytdlpUpdateAvailable = version_compare($ytdlp_version, $ytdlpVersion, '!=');
+    $ffmpegUpdateAvailable = version_compare($ffmpeg_version, $ffmpegVersion, '!=');
 
     $response = [
+        'update' => $appUpdateAvailable ,
+        'latest_version' => $latestVersion,
+        'download_url' => $downloadUrl,
+        'release_notes' => $releaseNotes,
+        "ytdlp" => [
+            'update' => $ytdlpUpdateAvailable,
+            'ytdlp_version' => $ytdlpVersion,
+            'download_url' => $downloadUrl, 
+            'release_notes' => $releaseNotes,
+        ],
+        "ffmpeg" => [
+            'update' => $ffmpegUpdateAvailable,
+            'ffmpeg_version' => $ffmpegVersion,
+            'download_url' => $downloadUrl, 
+            'release_notes' => $releaseNotes, 
+        ],
+    ];
+
+    if (!$appUpdateAvailable && !$ytdlpUpdateAvailable && !$ffmpegUpdateAvailable) {
+        $response['message'] = 'Bạn đang sử dụng phiên bản mới nhất.';
+    }
+} else {
+    $response = [
         'error' => true,
-        'message' => 'Không cung cấp'
+        'message' => 'Không cung cấp đủ thông tin phiên bản.'
     ];
 }
 
-// Trả về phản hồi dưới dạng JSON
+
 echo json_encode($response);
 
