83ac0813467b74a7faa6c568d3fa8f588112dc78
khanh <voanlaptrinh@gmail.com>
2024-07-01 14:14:24 +0700

    version_

--- version.php
+++ version.php
@@ -13,18 +13,30 @@ if (isset($_GET['current_version']) && isset($_GET['ytdlp_version']) && isset($_
     $currentVersion = $_GET['current_version'];
     $ytdlp_version = $_GET['ytdlp_version'];
     $ffmpeg_version = $_GET['ffmpeg_version'];
-    
-    if($_GET['os'] == 'Windows'){
-        $downloadUrl = "https://example.com/download/Windows/app-v1.2.0.apk";
-    }
-    
-    if($_GET['os'] == 'MacOs') {
+    if ($_GET['os'] == 'Windows64') {
+        $downloadUrl = "https://example.com/download/Windows64/app-v1.2.0.apk";
+        $downloadYtdlp = "https://example.com/downloadYtdlp/Windows64/app-v1.2.0.apk";
+        $downloadFfmpeg = "https://example.com/downloadFfmpeg/Windows64/app-v1.2.0.apk";
+    } elseif ($_GET['os'] == 'Windows86' || $_GET['os'] == 'Windows32') {
+        $downloadUrl = "https://example.com/download/Windows86-32/app-v1.2.0.apk";
+        $downloadYtdlp = "https://example.com/downloadYtdlp/Windows86-32/app-v1.2.0.apk";
+        $downloadFfmpeg = "https://example.com/downloadFfmpeg/Windows86-32/app-v1.2.0.apk";
+    } elseif ($_GET['os'] == 'MacOs') {
         $downloadUrl = "https://example.com/download/MacOS/app-v1.2.0.apk";
-    
+        $downloadYtdlp = "https://example.com/downloadYtdlp/MacOS/app-v1.2.0.apk";
+        $downloadFfmpeg = "https://example.com/downloadFfmpeg/MacOS/app-v1.2.0.apk";
+    } else {
+        $response = [
+            'error' => true,
+            'message' => 'OS không hợp lệ.'
+        ];
+        echo json_encode($response);
+        exit;
     }
     $appUpdateAvailable = version_compare($currentVersion, $latestVersion, '!=');
     $ytdlpUpdateAvailable = version_compare($ytdlp_version, $ytdlpVersion, '!=');
     $ffmpegUpdateAvailable = version_compare($ffmpeg_version, $ffmpegVersion, '!=');
+
 
     $response = [
         'update' => $appUpdateAvailable ,
@@ -34,13 +46,13 @@ if (isset($_GET['current_version']) && isset($_GET['ytdlp_version']) && isset($_
         "ytdlp" => [
             'update' => $ytdlpUpdateAvailable,
             'ytdlp_version' => $ytdlpVersion,
-            'download_url' => $downloadUrl, 
+            'download_url' => $downloadYtdlp, 
             'release_notes' => $releaseNotes,
         ],
         "ffmpeg" => [
             'update' => $ffmpegUpdateAvailable,
             'ffmpeg_version' => $ffmpegVersion,
-            'download_url' => $downloadUrl, 
+            'download_url' => $downloadFfmpeg, 
             'release_notes' => $releaseNotes, 
         ],
     ];
