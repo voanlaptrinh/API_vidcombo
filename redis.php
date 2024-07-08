9d2ebf9a8c180133df06f65736abeb2518e04a9d
kynndev <kynndev@gmail.com>
2024-07-08 11:09:07 +0700

    format code

---
+++ redis.php
@@ -0,0 +1,63 @@
+<?php
+
+class RedisCache
+{
+    public $keyCache;
+
+    /**
+     * @param mixed $keyCache
+     */
+    public function setKeyCache($keyCache)
+    {
+        $this->keyCache = $keyCache;
+    }
+
+    private $redis;
+
+    function __construct($keyCache, $sv = '15.235.216.178')
+    {
+        //49.12.120.177 : mate003
+        $this->keyCache = $keyCache;
+        $this->redis = new Redis();
+
+        try {
+            $this->redis->connect($sv, 2209);
+            $this->redis->auth('ngocquang14@AA');
+        } catch (Exception $e) {
+            $MaxRetries = 3;
+            for ($Counts = 1; $Counts <= $MaxRetries; $Counts++) {
+                try {
+                    $this->redis->connect($sv, 2209);
+                    $this->redis->auth('ngocquang14@AA');
+                } catch (Exception $e) {
+                    continue;
+                }
+            }
+        }
+
+    }
+
+    function getCache()
+    {
+        return $this->redis->get($this->keyCache);
+    }
+
+    function delCache()
+    {
+        return $this->redis->del($this->keyCache);
+    }
+
+    function setCache($cacheValue, $timeout)
+    {
+        if (is_array($cacheValue))
+            $cacheValue = json_encode($cacheValue);
+        return $this->redis->set($this->keyCache, $cacheValue, $timeout);
+    }
+
+    function setServer($sv)
+    {
+        $this->redis = new Redis();
+        $this->redis->connect($sv, 2209);
+        $this->redis->auth('ngocquang14@AA');
+    }
+}
