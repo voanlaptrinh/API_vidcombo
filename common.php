2703e92f700338acbfe69c324bff7518e16c290e
khanh <voanlaptrinh@gmail.com>
2024-08-07 15:02:34 +0700

    update common

--- common.php
+++ common.php
@@ -5,6 +5,16 @@ error_reporting(E_ALL);
 date_default_timezone_set('UTC');
 class Common
 {
+    public static $apiKey = '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';
+    public static $endpointSecret = 'whsec_xFaRWzhwBZ800CsllRVX89YHhxqPLja6';
+
+    public static $plans = array(
+        'plan1' => 'price_1Pl0EDJykwD5LYvp7ymIxuGP', // Id test
+        // 'plan1' => 'price_1PiultJykwD5LYvpJyb57WJ9',
+        'plan2' => 'price_1Piun4JykwD5LYvpVkpiWzuR',
+        'plan3' => 'price_1PiunkJykwD5LYvp0IGdnFUt',
+    );
+
     static function getDatabaseConnection() {
         try {
 
